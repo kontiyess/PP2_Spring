31c5b3d71cadfec09cf2f420ff251e0bf359407c
Esenbek Bilgekagan <kontiyess07@gmail.com>
2026-01-31 14:15:56 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ Practice 2/if_else/if_statement.py
@@ -0,0 +1,12 @@
+a = 33
+b = 200
+
+if b > a:
+  pass
+
+age = 16
+
+if age < 18:
+  pass # TODO: Add underage logic later
+else:
+  print("Access granted")
---
+++ Practice 2/if_else/short_hand_if.py
@@ -0,0 +1,7 @@
+a = 5
+b = 2
+if a > b: print("a is greater than b")
+
+a = 2
+b = 330
+print("A") if a > b else print("B")
